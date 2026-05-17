0380caae0207d25c4aa824c3ae0dcda1def2a940
Matthew Fong <matthewfong58@gmail.com>
2026-05-16 21:26:10 -0400

    First Setup

---
+++ textGame.py
@@ -0,0 +1,11 @@
+print("Welcome to your fantastical adventure!")
+name = input("What is your name adventurer?\nEnter your name: ")
+game_over = False
+
+inventory = []
+health = 100
+
+print("Health: ", health)
+print("Inventory: ", inventory)
+
+
