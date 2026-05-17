56a902fe8d25ddd9d5369a86c8c4b3ccd92edb98
Matthew Fong <matthewfong58@gmail.com>
2026-05-17 05:21:25 -0400

    run away and index error bugs fixed

--- textGame.py
+++ textGame.py
@@ -84,8 +84,7 @@ while True:
     while not game_over:
         #exploration mode
         if game_state == "exploration":
-            clear()
-            print("\nYou are wandering...")
+            print("\n\n\nYou are wandering...")
 
             print("1. Explore")
             print("2. Check status")
@@ -132,36 +131,46 @@ while True:
                 if sub == "1":
                     weapons = [i for i in inventory if items[i]["type"] == "weapon"]
 
-                    if len(weapons) == 0:
+                    if not weapons:
                         print("No weapons available.")
                     else:
                         for i, w in enumerate(weapons):
                             print(i + 1, w)
 
-                        pick = int(input("> ")) - 1
-                        equipment["weapon"] = weapons[pick]
-                        print("Equipped", weapons[pick])
+                        pick = input()
+
+                        if pick.isdigit():
+                            pick = int(pick) - 1
+
+                            if 0 <= pick < len(weapons):
+                                equipment["weapon"] = weapons[pick]
+                                print("Equipped", weapons[pick])
+                            else:
+                                print("Invalid choice.")
+                        else:
+                            print("Invalid input.")
                 
                 elif sub == "2":
                     armors = [i for i in inventory if items[i]["type"] == "armor"]
 
-                    if len(armors) == 0:
+                    if not armors:
                         print("No armor available.")
                     else:
                         for i, a in enumerate(armors):
                             print(i + 1, a)
 
-                        pick = int(input("> ")) - 1
-                        equipment["armor"] = armors[pick]
-                        print("Equipped", armors[pick])
+                        pick = input()
 
-                elif sub == "3":
-                    equipment["weapon"] = None
-                    print("Weapon unequipped.")
+                        if pick.isdigit():
+                            pick = int(pick) - 1
 
-                elif sub == "4":
-                    equipment["armor"] = None
-                    print("Armor unequipped.")
+                            if 0 <= pick < len(armors):
+                                equipment["armor"] = armors[pick]
+                                print("Equipped", armors[pick])
+                            else:
+                                print("Invalid choice.")
+                        else:
+                            print("Invalid input.")
         
         #combat mode
         elif game_state == "combat":
@@ -173,58 +182,68 @@ while True:
 
             choice = input()
 
-            #ARMOR CALC 
-            armor_value = 0
-            if equipment["armor"]:
-                armor_value = items[equipment["armor"]]["value"]
+            armor_value = items[equipment["armor"]]["value"] if equipment["armor"] else 0
+            weapon_damage = items[equipment["weapon"]]["value"] if equipment["weapon"] else 2
 
-            #WEAPON CALC
-            weapon_damage = 2
-            if equipment["weapon"]:
-                weapon_damage = items[equipment["weapon"]]["value"]
-            
             if enemy is None:
+                game_state = "exploration"
                 continue
 
+            # PLAYER TURN
             if choice == "1":
                 print("You attack!")
-
                 enemy_hp -= weapon_damage
-                print("You deal", weapon_damage, "damage.")
+                print("You deal", weapon_damage)
 
                 if enemy_hp <= 0:
-                    print("You defeated the enemy!")
-                    game_state = "exploration"
+                    print("Enemy defeated!")
                     enemy = None
+                    game_state = "exploration"
                     continue
 
+            elif choice == "2":
+                print("You ran away!")
+                enemy = None
+                enemy_hp = 0
+                game_state = "exploration"
+                continue
+
+            elif choice == "3":
+                potions = [i for i in inventory if items[i]["type"] == "heal"]
+
+                if not potions:
+                    print("No potions!")
+                print("Choose potion:")
+
+                for i, p in enumerate(potions):
+                    print(i + 1, p)
+
+                pick = input()
+
+                if not pick.isdigit():
+                    print("Invalid input.")
+
+                else:
+                    pick = int(pick) - 1
+                    if 0 <= pick < len(potions):
+                        potion = potions[pick]
+
+                        heal_amount = items[potion]["value"]
+                        health += heal_amount
+
+                        inventory.remove(potion)
+
+                        print("Healed", heal_amount)
+
+                    else:
+                        print("Invalid choice.")
+
+            # ENEMY TURN (ONLY IF STILL IN COMBAT)
             if game_state == "combat" and enemy is not None:
                 damage = max(0, enemies[enemy]["damage"] - armor_value)
                 health -= damage
                 print("Enemy hits you for", damage)
 
-            elif choice == "2":
-                print("You ran away!")
-                game_state = "exploration"
-
-            elif choice == "3":
-                potions = [i for i in inventory if items[i]["type"] == "heal"]
-
-                if len(potions) == 0:
-                    print("No potions!")
-                else:
-                    print("Choose potion:")
-                    for i, p in enumerate(potions):
-                        print(i + 1, p)
-
-                    pick = int(input()) - 1
-                    potion = potions[pick]
-                    
-                    heal_amount = items[potion]["value"]
-                    health += heal_amount
-                    inventory.remove(potion)
-
-                    print("You used", potion, "and healed", heal_amount)
 
         if health <= 0: 
             print("You died!")
